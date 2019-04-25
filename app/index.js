c6090d71968d3f462536abe43163af693b016078
a8745324@163.com <you@example.com>
2019-04-26 00:11:27 +0800

    re-425

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import Banner from './components/banner'
+export default class index extends Component {
+  render() {
+    return (
+     <Banner></Banner>
+    )
+  }
+}
